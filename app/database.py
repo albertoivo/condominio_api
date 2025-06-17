9765b8e28dd7f1e45ceb8bd9ba87b7cf188f62e6
Alberto Ivo Vieira <albertoivo@gmail.com>
2025-06-17 18:29:19 -0300

    Set default DATABASE_URL to use SQLite for local development

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from app.models.base import Base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
