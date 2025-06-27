41114bac9db0e72140147d1bfe4048a19cec46d4
Alberto Ivo Vieira <albertoivo@gmail.com>
2025-06-27 15:11:51 -0300

    Add abstract base model and move id column to Base class

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,6 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     nome: Mapped[str] = mapped_column(String(100))
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
